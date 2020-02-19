228248f925732cbf498ecb1fd72010df1db18778
EdixonAlberto <edixonalbertto@gmail.com>
2020-02-18 23:58:49 -0400

    project start

---
+++ gui.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Libraries;
+
+use Exception;
+
+class Gui
+{
+    /**
+        GUI v4
+     */
+    public static function create(): string
+    {
+        $code = sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 65535),
+            mt_rand(0, 65535),
+            mt_rand(0, 65535),
+            mt_rand(16384, 20479),
+            mt_rand(32768, 49151),
+            mt_rand(0, 65535),
+            mt_rand(0, 65535),
+            mt_rand(0, 65535)
+        );
+        return $code;
+    }
+
+    public static function validate(string $gui): bool
+    {
+        if (preg_match(
+            '/^[a-z0-9]{8}(\-[a-z0-9]{4}){3}\-[a-z0-9]{12}$/',
+            $gui
+        )) return true;
+        else throw new Exception('id incorrect', 400);
+    }
+}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+require 'gui.php';
+
+
+var_dump(Gui.create());
